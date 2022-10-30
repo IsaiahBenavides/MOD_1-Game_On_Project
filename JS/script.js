df3e1bfc0ab68c4dd1c4be3be83d5d7a0f91eba3
Isaiah Bens <benavidesisaiah@outlook.com>
2022-10-29 20:29:12 -0400

    Start and restart button

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,32 @@
+// Quereies
+const startBtn = document.querySelector(`#startButton`);
+const gameField = document.querySelector(`#gameContainer`);
+const restartBtn = document.querySelector(`#restartButton`);
+
+// Event listeners
+startBtn.addEventListener(`click`, ()=>{
+    startBtn.classList.add(`hide`);
+    startBtn.classList.remove(`button-87`);
+
+    restartBtn.classList.add(`button-87`);
+    restartBtn.classList.remove(`hide`);
+    
+});
+
+restartBtn.addEventListener(`click`, ()=>{
+    location.reload()
+});
+
+// The game itself
+const gameParams = {
+    rows: 8,
+    columns:8,
+    bombs: 10,
+    bombImg: `💥`,
+    lose: false,
+    warnings: {1: `blue`, 2: `yellow`, 3: `red`, 4: `orange`, 5: `green`, 6: `purple`, 7: `aqua`, 8: `chartreuse`}
+};
+
 const game = {
     
-}
+};
