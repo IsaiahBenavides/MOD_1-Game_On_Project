8e0405ef9300c0ee983034a990d35906663774dc
Isaiah Bens <benavidesisaiah@outlook.com>
2022-11-03 12:14:48 -0400

    Hole randomizer working

--- Whack-a-mole-Copy/Script/script.js
+++ Whack-a-mole-Copy/Script/script.js
@@ -8,19 +8,19 @@ const timer = document.querySelector(`#time`);
 
 //Mole Holes
     const moleHill = document.querySelector(`#moleHill`);
-    const hole1 = document.querySelector(`#h1Hole`);
-    const hole2 = document.querySelector(`#h2Hole`);
-    const hole3 = document.querySelector(`#h3Hole`);
-    const hole4 = document.querySelector(`#h4Hole`);
-    const hole5 = document.querySelector(`#h5Hole`);
-    const hole6 = document.querySelector(`#h6Hole`);
-    const hole7 = document.querySelector(`#h7Hole`);
-    const hole8 = document.querySelector(`#h8Hole`);
-    const hole9 = document.querySelector(`#h9Hole`);
+    const hole0 = document.querySelector(`#h1Hole`);
+    const hole1 = document.querySelector(`#h2Hole`);
+    const hole2 = document.querySelector(`#h3Hole`);
+    const hole3 = document.querySelector(`#h4Hole`);
+    const hole4 = document.querySelector(`#h5Hole`);
+    const hole5 = document.querySelector(`#h6Hole`);
+    const hole6 = document.querySelector(`#h7Hole`);
+    const hole7 = document.querySelector(`#h8Hole`);
+    const hole8 = document.querySelector(`#h9Hole`);
     const moleHillArr = [
-        hole1, hole2, hole3, 
-        hole4, hole5, hole6, 
-        hole7, hole8, hole9
+        hole0, hole1, hole2, 
+        hole3, hole4, hole5, 
+        hole6, hole7, hole8
     ];
 
 // Variables 
@@ -31,7 +31,7 @@ let lastPopUp = null;
 
 const game = {
     gameOver: false,
-    currentHole: hole1,
+    currentHole: hole0,
     currentMole: `Images/mole1Point.png`,
     timeKeeper: ()=>{
         let countDown = 5;
@@ -61,7 +61,31 @@ const game = {
         }
     },
 
+    holeRandomizer: ()=>{
+        let holeChance = Math.ceil(Math.random()*9).toFixed(0);
+        if(holeChance <= 9 && holeChance > 8){
+            game.currentHole = hole8
+        }else if(holeChance <= 8 && holeChance > 7){
+            game.currentHole = hole7
+        }else if(holeChance <= 7 && holeChance > 6){
+            game.currentHole = hole6
+        }else if(holeChance <= 6 && holeChance > 5){
+            game.currentHole = hole5
+        }else if(holeChance <= 5 && holeChance > 4){
+            game.currentHole = hole4
+        }else if(holeChance <= 4 && holeChance > 3){
+            game.currentHole = hole3
+        }else if(holeChance <= 3 && holeChance > 2){
+            game.currentHole = hole2
+        }else if(holeChance <= 2 && holeChance > 1){
+            game.currentHole = hole1
+        }else{
+            game.currentHole = hole0
+        };
+    },
+
     swapOut: ()=>{
+        game.holeRandomizer()
         game.moleRandomizer()
         game.currentHole.src = game.currentMole
     },
